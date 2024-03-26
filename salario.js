ec1ae0230678fa00d0a5933bbdaa96674e04ba7f
Seu Nome <seu@email.com>
2024-03-26 00:16:02 -0300

    Atualizando

--- salario.js
+++ salario.js
@@ -11,22 +11,23 @@ rl.question('Digite o salário bruto: ', (salarioBrutoInput) => {
     // Convertendo entrada de string para número
     let salarioBruto =  parseFloat(salarioBrutoInput)
     let beneficios = parseFloat(beneficiosInput)
+    let salarioLiquido
 
     if (salarioBruto <= 1100.00) {
         // Aqui atribuímos a aliquita de 5% sobre o salário
         let imposto = (5/100)*salarioBruto
-        let salario = (salarioBruto-imposto) + beneficios
-        console.log(`O salário líquido do funcionário é: R$${salario}`)
+        salarioLiquido = (salarioBruto-imposto) + beneficios
+        console.log(`O salário líquido do funcionário é: R$${salarioLiquido}`)
     }else if (salarioBruto >= 1100.01 && salarioBruto <= 2500.00) {
         // Aqui atribuímos a aliquita de 10% sobre o salário
         let imposto = (10/100)*salarioBruto
-        let salario = (salarioBruto-imposto) + beneficios
-        console.log(`O salário líquido do funcionário é: R$${salario}`)
+        salarioLiquido = (salarioBruto-imposto) + beneficios
+        console.log(`O salário líquido do funcionário é: R$${salarioLiquido}`)
     }else {
         // Aqui atribuímos a aliquita de 15% sobre o salário        
         let imposto = (15/100)*salarioBruto
-        let salario = (salarioBruto-imposto) + beneficios
-        console.log(`O salário líquido do funcionário é: R$${salario}`)
+        salarioLiquido = (salarioBruto-imposto) + beneficios
+        console.log(`O salário líquido do funcionário é: R$${salarioLiquidoo}`)
     }
     rl.close();
   });
